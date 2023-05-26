2256228dc89aca1420ba5749710780b8324caa60
bernasali <65134190+bernasali@users.noreply.github.com>
2023-05-26 15:02:03 +0300

    back-end change

--- src/main/java/com/imgclassificator/repository/ImageRepository.java
+++ src/main/java/com/imgclassificator/repository/ImageRepository.java
@@ -7,16 +7,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
-   // Card card = cardRepository.save(new Card(createCardResource.getCardType(), cardOwner.get()));
-
-   // Custom query method to find an image by ID
 
    ImageEntity findById(long id);
    ImageEntity findByUrl(String url);
-   boolean urlCheck(String url);
-   // Custom query method to find all images with specific tags
+
    //List<ImageEntity> findByTagsIn(List<TagEntity> tags);
 
-    // Custom query method to implement pagination
+
    //Page<ImageEntity> findAll(SpringDataWebProperties.Pageable pageable);
 }
